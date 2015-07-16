e19e63599a12e53d60f02337447e54a66a422dab
kellysutton <michael.k.sutton@gmail.com>
2015-07-15 23:00:14 -0700

    Fix our md5 inclusion

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
   name: 'ember-cli-imgix',
 
   included: function(app) {
-    this.app.import(app.bowerDirectory + '/blueimp-md5/js/md5.js');
+    this.app.import(app.bowerDirectory + '/md5/build/md5.min.js');
     this.app.import(app.bowerDirectory + '/lodash/lodash.js');
     this.app.import(app.bowerDirectory + '/uri.js/src/URI.js');
     this.app.import(app.bowerDirectory + '/imgix-core-js/dist/imgix-core-js.umd.js');
