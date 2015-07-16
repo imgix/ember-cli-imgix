3fdacf59ff1833d0a55bd0a768671df2debea48a
kellysutton <michael.k.sutton@gmail.com>
2015-07-15 23:10:33 -0700

    Bump some deps

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ module.exports = {
 
   included: function(app) {
     this.app.import(app.bowerDirectory + '/md5/build/md5.min.js');
-    this.app.import(app.bowerDirectory + '/lodash/lodash.js');
     this.app.import(app.bowerDirectory + '/uri.js/src/URI.js');
     this.app.import(app.bowerDirectory + '/imgix-core-js/dist/imgix-core-js.umd.js');
     this.app.import(app.bowerDirectory + '/ember-cli-imgix-core-js-shim/imgix-core-js-shim.js');
