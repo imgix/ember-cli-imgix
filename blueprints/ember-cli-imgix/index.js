1bc960dc5b6e78ac30ec0db01bb961a17197fcfb
Paul Straw <paulstraw@paulstraw.com>
2016-02-29 13:30:51 -0800

    Update to imgix-core-js 0.3.1

--- blueprints/ember-cli-imgix/index.js
+++ blueprints/ember-cli-imgix/index.js
@@ -4,7 +4,7 @@ module.exports = {
   afterInstall: function() {
     return this.addBowerPackagesToProject([
       { name: 'ember-cli-imgix-core-js-shim', target: '0.0.2' },
-      { name: 'imgix-core-js', target: '0.2.4' }
+      { name: 'imgix-core-js', target: '0.3.1' }
     ]);
   }
 };
--- index.js
+++ index.js
@@ -11,6 +11,7 @@ module.exports = {
   included: function(app) {
     this.app.import(app.bowerDirectory + '/md5/build/md5.min.js');
     this.app.import(app.bowerDirectory + '/uri.js/src/URI.js');
+    this.app.import(app.bowerDirectory + '/js-base64/base64.js');
     this.app.import(app.bowerDirectory + '/imgix-core-js/dist/imgix-core-js.umd.js');
     this.app.import(app.bowerDirectory + '/ember-cli-imgix-core-js-shim/imgix-core-js-shim.js');
   }
