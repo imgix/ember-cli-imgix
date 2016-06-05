2cb34c2196fc8fca4e74a1442fc48cca3e21213b
Reid Beels <mail@reidbeels.com>
2016-06-05 18:50:50 +0200

    Support setting the IMG's `alt` attribute

--- addon/components/imgix-image-element.js
+++ addon/components/imgix-image-element.js
@@ -5,7 +5,7 @@ import ImgixPathBehavior from '../mixins/imgix-path-behavior';
 export default Ember.Component.extend(ImgixPathBehavior, ResizeMixin, {
   tagName: 'img',
   layout: null,
-  attributeBindings: ['src', 'crossorigin', 'style'],
+  attributeBindings: ['src', 'crossorigin', 'style', 'alt'],
 
   useParentWidth: true,
 });
