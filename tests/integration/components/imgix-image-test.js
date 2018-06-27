ee5ad37f40eb891d1b64db3dd586e6196b6652eb
Frederick Fogerty <frederick.fogerty@gmail.com>
2018-06-28 10:48:24 +1200

    chore: merge unit and integration tests

--- tests/integration/components/imgix-image-test.js
+++ tests/integration/components/imgix-image-test.js
@@ -9,26 +9,47 @@ moduleForComponent('imgix-image', 'Integration | Component | imgix image', {
 
 test('it renders', function(assert) {
   this.render(hbs`{{imgix-image path="/users/1.png"}}`);
+  assert.ok(this.$());
+});
 
-  assert.equal(this.$().text().trim(), '');
-  assert.equal(this.$('img').length, 1);
+test('it renders event more better', function(assert) {
+  this.render(
+    hbs`<div style='width:200px;height:200px;'>{{imgix-image path='/users/1.png' }}</div>`
+  );
+
+  let url = new URL(this.$('img').attr('src'));
+  assert.equal(
+    this.$()
+      .text()
+      .trim(),
+    ''
+  );
+  assert.equal(url.pathname, '/users/1.png');
 });
 
 test('it renders the correct path', function(assert) {
   this.render(hbs`{{imgix-image path="/users/1.png"}}`);
 
-  assert.ok(this.$('img').attr('src').indexOf("https://assets.imgix.net/users/1.png") > -1);
-  assert.ok(this.$('img').attr('src').indexOf("w=1280") > -1);
+  assert.ok(
+    this.$('img')
+      .attr('src')
+      .indexOf('https://assets.imgix.net/users/1.png') > -1
+  );
+  assert.ok(
+    this.$('img')
+      .attr('src')
+      .indexOf('w=1280') > -1
+  );
 });
 
 test('it builds the default URL', function(assert) {
   this.render(hbs`{{imgix-image path="/users/1.png"}}`);
   let url = URI(this.$('img').attr('src'));
 
-  assert.equal(url.search(true).w, "1280");
-  assert.equal(url.pathname(), "/users/1.png");
-  assert.equal(url.search(true).fit, "crop");
-  assert.equal(url.search(true).crop, "faces");
+  assert.equal(url.search(true).w, '1280');
+  assert.equal(url.pathname(), '/users/1.png');
+  assert.equal(url.search(true).fit, 'crop');
+  assert.equal(url.search(true).crop, 'faces');
 });
 
 test('it maintains any query parameters passed in', function(assert) {
@@ -36,36 +57,45 @@ test('it maintains any query parameters passed in', function(assert) {
   this.render(hbs`{{imgix-image path="/users/1.png?sat=100"}}`);
 
   let url = URI(this.$('img').attr('src'));
-  assert.equal(url.search(true).sat, "100");
-  assert.equal(url.search(true).w, "1280");
+  assert.equal(url.search(true).sat, '100');
+  assert.equal(url.search(true).w, '1280');
 });
 
 test('it renders with an aspect ratio', function(assert) {
   this.render(hbs`{{imgix-image path="/users/1.png" aspectRatio=1.3333}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(
+    this.$()
+      .text()
+      .trim(),
+    ''
+  );
   let url = URI(this.$('img').attr('src'));
 
-  assert.equal(url.search(true).w, "1280");
-  assert.equal(url.search(true).h, "960");
+  assert.equal(url.search(true).w, '1280');
+  assert.equal(url.search(true).h, '960');
 });
 
 test('it respects passed in `crop` and `fit` values', function(assert) {
   assert.expect(2);
-  this.render(hbs`{{imgix-image path="/users/1.png?sat=100&fit=min&crop=top,left"}}`);
+  this.render(
+    hbs`{{imgix-image path="/users/1.png?sat=100&fit=min&crop=top,left"}}`
+  );
 
   let url = URI(this.$('img').attr('src'));
-  assert.equal(url.search(true).fit, "min");
-  assert.equal(url.search(true).crop, "top,left");
+  assert.equal(url.search(true).fit, 'min');
+  assert.equal(url.search(true).crop, 'top,left');
 });
 
 test('it respects `crop` and `fit` values passed as attributes', function(assert) {
   assert.expect(2);
-  this.render(hbs`{{imgix-image path="/users/1.png" crop="top,left" fit="min"}}`);
+  this.render(
+    hbs`{{imgix-image path="/users/1.png" crop="top,left" fit="min"}}`
+  );
 
   let url = URI(this.$('img').attr('src'));
-  assert.equal(url.search(true).crop, "top,left");
-  assert.equal(url.search(true).fit, "min");
+  assert.equal(url.search(true).crop, 'top,left');
+  assert.equal(url.search(true).fit, 'min');
 });
 
 test('it respects `auto` values passed as attributes', function(assert) {
@@ -73,13 +103,24 @@ test('it respects `auto` values passed as attributes', function(assert) {
   this.render(hbs`{{imgix-image path="/users/1.png" auto="compress,enhance"}}`);
 
   let url = URI(this.$('img').attr('src'));
-  assert.equal(url.search(true).auto, "compress,enhance");
+  assert.equal(url.search(true).auto, 'compress,enhance');
 });
 
 test('it allows setting the alt attribute', function(assert) {
-
   this.render(hbs`{{imgix-image path="/users/1.png" alt="User 1"}}`);
 
   let alt = this.$('img').attr('alt');
   assert.equal(alt, 'User 1');
+});
+
+test('it allows passing ANY imgix parameter as an option hash', function(assert) {
+  assert.expect(2);
+  this.render(
+    hbs`<div style='width:1280px;height:200px;'>{{imgix-image path='/users/1.png' options=(hash exp=20 invert=true)}}</div>`
+  );
+
+  let url = new URL(this.$('img').attr('src'));
+
+  assert.equal(url.searchParams.get('exp'), 20);
+  assert.equal(url.searchParams.get('invert'), 'true');
 });
--- tests/unit/components/imgix-image-test.js
+++ tests/unit/components/imgix-image-test.js
@@ -1,31 +1,19 @@
-import Ember from 'ember';
+import { get, setProperties } from '@ember/object';
 import { moduleForComponent, test } from 'ember-qunit';
-/* global URI */
+import config from 'ember-get-config';
 
 moduleForComponent('imgix-image', 'Unit | Component | imgix image', {
-  // Specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar'],
-  unit: true
+  unit: true,
+  needs: ['service:unifiedEventHandler']
 });
-
-let defaultOptions = {
-  _config: {
-    APP: {
-      imgix: {
-        source: 'assets.imgix.net'
-      }
-    }
-  }
-};
 
 test('it renders', function(assert) {
   assert.expect(2);
 
   // Creates the component instance
-  var component = this.subject(defaultOptions);
-  component.set('path', "/users/1.png");
+  var component = this.subject();
+  setProperties(component, { path: '/users/1.png' });
   assert.equal(component._state, 'preRender');
-
 
   // Renders the component to the page
   this.render();
@@ -33,51 +21,64 @@ test('it renders', function(assert) {
 });
 
 test('it does not throw an exception when given an undefined path', function(assert) {
-  var component = this.subject(defaultOptions);
-  component.set('path', undefined);
+  var component = this.subject();
+  setProperties(component, { path: undefined });
   this.render();
   assert.equal(component._state, 'inDOM');
 });
 
 test('it sets the source correctly', function(assert) {
-  var component = this.subject(defaultOptions);
-  component.setProperties({
-    path: "/users/1.png",
-    _width: 400,
-    _height: 300
+  const component = this.subject();
+
+  setProperties(component, {
+    path: '/users/1.png',
+    _dpr: 1
   });
 
-  let url = URI(component.get('src'));
-  assert.equal(defaultOptions._config.APP.imgix.source, url.host());
-  assert.equal('https', url.protocol());
-  assert.equal('/users/1.png', url.path());
-  assert.ok(url.hasQuery("w", 400));
-  assert.ok(url.hasQuery("h", 300));
-  assert.ok(url.hasQuery("dpr", 1));
-  assert.ok(url.hasQuery("crop", "faces"));
-  assert.ok(url.hasQuery("fit", "crop"));
+  component.didResize(400, 300);
+
+  assert.ok(component.get('src'));
+
+  const url = new window.URL(component.get('src'));
+
+  assert.equal(config.APP.imgix.source, url.host);
+  assert.equal('https:', url.protocol);
+  assert.equal('/users/1.png', url.pathname);
+  assert.equal(url.searchParams.get('w'), 400);
+  assert.equal(url.searchParams.get('h'), 300);
+  assert.equal(url.searchParams.get('dpr'), 1);
+  assert.equal(url.searchParams.get('crop'), 'faces');
+  assert.equal(url.searchParams.get('fit'), 'crop');
 });
 
 test('it does not generate a source without a width', function(assert) {
-  var component = this.subject(defaultOptions);
-  component.setProperties({
-    path: "/users/1.png",
+  const component = this.subject();
+
+  setProperties(component, {
+    path: '/users/1.png',
     _width: null
   });
 
-  assert.equal(null, component.get('src'));
+  assert.equal(null, get(component, 'src'));
 });
 
 test('it respects the pixel step', function(assert) {
-  let component = this.subject(defaultOptions);
-  component.setProperties({
-    path: "/users/1.png",
-    pixelStep: 10,
-    element: Ember.Object.create({
-      clientWidth: 405
-    })
+  const component = this.subject();
+
+  setProperties(component, {
+    path: '/users/1.png',
+    pixelStep: 10
   });
 
-  let url = URI(component.get('src'));
-  assert.equal(url.search(true).w, "410", "Expected a step up to 410, instead stepped to: ", url.search(true).w);
+  component.didResize(405, 300);
+
+  const url = new window.URL(component.get('src'));
+
+  assert.equal(
+    url.searchParams.get('w'),
+    '410',
+    `Expected a step up to 410, instead stepped to: ${url.searchParams.get(
+      'w'
+    )}`
+  );
 });
