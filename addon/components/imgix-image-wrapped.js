79b0d36185f83b361c970f18dc8a1e7da9531dfe
Frederick Fogerty <frederick.fogerty@gmail.com>
2018-07-06 10:32:30 +1200

    chore(imgix-image-wrapped): add deprecation notice

--- addon/components/imgix-image-wrapped.js
+++ addon/components/imgix-image-wrapped.js
@@ -1,9 +1,21 @@
 import Component from '@ember/component';
+import { deprecate } from '@ember/application/deprecations';
 import ResizeMixin from 'ember-resize-mixin/main';
 import layout from '../templates/components/imgix-image-wrapped';
 import ImgixPathBehavior from '../mixins/imgix-path-behavior';
 
 export default Component.extend(ResizeMixin, ImgixPathBehavior, {
   layout: layout,
-  classNames: ['imgix-image-wrap']
+  classNames: ['imgix-image-wrap'],
+  init() {
+    this._super(...arguments);
+    deprecate(
+      'imgix-image-wrapped is deprecated and will be removed in ember-cli-imgix@2. Please migrate to using imgix-image instead.',
+      false,
+      {
+        id: 'ember-cli-imgix/imgix-image-wrapper-deprecation',
+        until: '2.0.0'
+      }
+    );
+  }
 });
