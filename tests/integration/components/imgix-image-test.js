1ac8b7eb099f185fab99e31a7447dc4c8a89a138
Frederick Fogerty <frederick.fogerty@gmail.com>
2018-10-16 09:13:32 +1300

    test(ar): add tests for aspect-ratio

--- tests/integration/components/imgix-image-test.js
+++ tests/integration/components/imgix-image-test.js
@@ -1,4 +1,5 @@
-import { module, test, skip } from 'qunit';
+/* global Ember global */
+import { module, test } from 'qunit';
 
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
@@ -52,35 +53,127 @@ module('Integration | Component | imgix image', function(hooks) {
     });
   });
 
-  // TODO: Upgrade AR from decimal to ratio
-
-  test('it renders with an aspect ratio', function(assert) {
-    this.render(
-      hbs`<div>{{imgix-image path="/users/1.png" aspectRatio=1.3333}}</div>`
-    );
-
-    assert.equal(
-      this.$()
-        .text()
-        .trim(),
-      ''
-    );
-
-    let uri = new URI(this.$('img').attr('src'));
-
-    assert.equal(uri.getQueryParamValue('w'), '1250');
-    assert.equal(uri.getQueryParamValue('h'), '937');
-  });
-
   module('aspect ratio', function() {
     module(`valid ARs`, function() {
       const testValidAR = ({ ar, arDecimal }) => {
-        test(`it generates the correct srcset heights for a valid AR ()`, function(assert) {});
+        test(`it generates the correct srcset heights for a valid AR (${ar})`, async function(assert) {
+          const removeFallbackSrcSet = srcSets => srcSets.slice(0, -1);
+          const content = Ember.HTMLBars.compile(
+            `<div>{{imgix-image path="/users/1.png" options=(hash ar="${ar}")}}</div>`
+          );
+          await render(content);
+          const srcSet = this.$('img').attr('srcset');
+          const srcSets = srcSet.split(',').map(v => v.trim());
+          const srcSetUrls = srcSets.map(srcSet => srcSet.split(' ')[0]);
+          removeFallbackSrcSet(srcSetUrls).forEach(srcSetUrl => {
+            const srcSetURI = new URI(srcSetUrl);
+            const w = srcSetURI.getQueryParamValue('w');
+            const h = srcSetURI.getQueryParamValue('h');
+            assert.equal(h, Math.ceil(w / arDecimal));
+            assert.ok(w);
+            assert.ok(h);
+          });
+        });
+      };
+      [
+        ['1:1', '1'],
+        ['1.1:1', '1.1'],
+        ['1.12:1', '1.12'],
+        ['1.123:1', '1.123'],
+        ['1:1.1', '0.9090909090909091'],
+        ['1:1.12', '0.8928571428571428'],
+        ['1.1:1.1', '1'],
+        ['1.123:1.123', '1'],
+        ['11.123:11.123', '1']
+      ].forEach(([validAR, validARDecimal]) =>
+        testValidAR({
+          ar: validAR,
+          arDecimal: validARDecimal
+        })
+      );
+
+      test(`it generates the correct srcset heights for a valid AR using the deprecated aspectRatio option`, async function(assert) {
+        const ar = '1.1:1';
+        const arDecimal = '1.1';
+        const removeFallbackSrcSet = srcSets => srcSets.slice(0, -1);
+        const content = Ember.HTMLBars.compile(
+          `<div>{{imgix-image path="/users/1.png" aspectRatio="${ar}"}}</div>`
+        );
+        await render(content);
+        const srcSet = this.$('img').attr('srcset');
+        const srcSets = srcSet.split(',').map(v => v.trim());
+        const srcSetUrls = srcSets.map(srcSet => srcSet.split(' ')[0]);
+        removeFallbackSrcSet(srcSetUrls).forEach(srcSetUrl => {
+          const srcSetURI = new URI(srcSetUrl);
+          const w = srcSetURI.getQueryParamValue('w');
+          const h = srcSetURI.getQueryParamValue('h');
+          assert.equal(h, Math.ceil(w / arDecimal));
+          assert.ok(w);
+          assert.ok(h);
+        });
+      });
+    });
+
+    module('invalid ARs', function() {
+      const testInvalidAR = ar => {
+        test(`height should not be set when an invalid aspectRatio (${ar}) is passed`, async function(assert) {
+          const oldConsole = global.console;
+          global.console = { warn: () => {} };
+
+          const removeFallbackSrcSet = srcSets => srcSets.slice(0, -1);
+
+          const content = Ember.HTMLBars.compile(
+            `<div>{{imgix-image path="/users/1.png" options=(hash ar="${ar}")}}</div>`
+          );
+          await render(content);
+
+          const srcSet = this.$('img').attr('srcset');
+          const srcSets = srcSet.split(',').map(v => v.trim());
+          const srcSetUrls = srcSets.map(srcSet => srcSet.split(' ')[0]);
+          removeFallbackSrcSet(srcSetUrls).forEach(srcSetUrl => {
+            const srcSetURI = new URI(srcSetUrl);
+            const w = srcSetURI.getQueryParamValue('w');
+            const h = srcSetURI.getQueryParamValue('h');
+
+            assert.ok(w);
+            assert.notOk(h);
+          });
+
+          global.console = oldConsole;
+        });
       };
 
-      testValidAR('1:1', 1);
-      test(`it generates the correct srcset heights for a valid AR ()`, function(assert) {
-        assert.ok(true);
+      [
+        '4x3',
+        '4:',
+        'blah:1:1',
+        'blah1:1',
+        '1x1',
+        '1:1blah',
+        '1:blah1'
+      ].forEach(invalidAR => testInvalidAR(invalidAR));
+    });
+
+    test('srcsets should not have a height set when aspectRatio is not set', async function(assert) {
+      await render(hbs`<div>{{imgix-image path="/users/1.png"}}</div>`);
+
+      const srcSet = this.$('img').attr('srcset');
+      const srcSets = srcSet.split(',').map(v => v.trim());
+      const srcSetUrls = srcSets.map(srcSet => srcSet.split(' ')[0]);
+      srcSetUrls.forEach(srcSetUrl => {
+        const srcSetURI = new URI(srcSetUrl);
+        const h = srcSetURI.getQueryParamValue('h');
+        assert.notOk(h);
+      });
+    });
+
+    test('the generated src should not have ar included', async function(assert) {
+      await render(
+        hbs`<div>{{imgix-image path="/users/1.png" options=(hash ar="2:1")}}</div>`
+      );
+
+      expectSrcsTo(this.$, (_, uri) => {
+        assert.notOk(uri.getQueryParamValue('ar'));
       });
     });
   });
