70960b576ed63c80adafe91c5dc252e695f480fb
Frederick Fogerty <frederick.fogerty@gmail.com>
2018-10-20 13:31:02 +1300

    chore(ember-try): update scenarios to use npm and current versions

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,60 +7,28 @@ module.exports = {
       name: 'ember-lts-2.16',
       npm: {
         devDependencies: {
-          'ember-source': '~2.16.0'
+          'ember-source': 'lts'
         }
       }
     },
     {
-      name: 'ember-release',
-      bower: {
-        dependencies: {
-          ember: 'components/ember#release'
-        },
-        resolutions: {
-          ember: 'release'
-        }
-      },
+      name: 'ember-stable',
       npm: {
         devDependencies: {
-          'ember-source': null
+          'ember-source': 'latest'
         }
       }
     },
     {
       name: 'ember-beta',
-      bower: {
-        dependencies: {
-          ember: 'components/ember#beta'
-        },
-        resolutions: {
-          ember: 'beta'
-        }
-      },
       npm: {
         devDependencies: {
-          'ember-source': null
+          'ember-source': 'beta'
         }
       }
     },
     {
-      name: 'ember-canary',
-      bower: {
-        dependencies: {
-          ember: 'components/ember#canary'
-        },
-        resolutions: {
-          ember: 'canary'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-default',
+      name: 'current',
       npm: {
         devDependencies: {}
       }
