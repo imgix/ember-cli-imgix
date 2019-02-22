6aa58f1a883c4c731df2acb2c8e37adda3475fac
Sherwin H <sherwin@imgix.com>
2019-02-22 14:44:01 -0800

    test: add tests for global default params on background-mode

--- tests/integration/components/imgix-background-test.js
+++ tests/integration/components/imgix-background-test.js
@@ -6,9 +6,11 @@ import ImgixBG from 'ember-cli-imgix/components/imgix-bg';
 
 import { setupRenderingTest } from 'ember-qunit';
 import { render, find } from '@ember/test-helpers';
+import { assign } from '@ember/polyfills';
+import config from 'ember-get-config';
 import hbs from 'htmlbars-inline-precompile';
 import URI from 'jsuri';
-// import config from 'ember-get-config';
+
 const isIE = (() => {
   const ua = window.navigator.userAgent;
   const isIE = /MSIE|Trident/.test(ua);
@@ -260,19 +262,20 @@ module('Integration | Component | imgix background', function(hooks) {
     const oldDPR = global.devicePixelRatio;
     global.devicePixelRatio = 2;
 
-    await render(hbs`
-        <div>
-          <style>.imgix-bg { width: 10px; height: 10px; }</style>
-          {{#imgix-bg
-            path="/users/1.png"
-            options=(hash
-              dpr=3
-            )
-          }}
-            Content
-          {{/imgix-bg}}
-        </div>
-          `);
+    await render(
+      hbs`
+      <div>
+        <style>.imgix-bg { width: 10px; height: 10px; }</style>
+        {{#imgix-bg
+          path="/users/1.png"
+          options=(hash
+            dpr=3
+          )
+        }}
+          Content
+        {{/imgix-bg}}
+      </div>
+      `);
 
     const bgImageSrcURL = findURI();
 
@@ -310,4 +313,219 @@ module('Integration | Component | imgix background', function(hooks) {
 
     assert.equal(find('.imgix-bg').getAttribute('alt'), 'Test alt');
   });
+
+  module('application config', function(hooks) {
+    hooks.beforeEach(function() {
+      this.initialAppConfig = assign({}, config.APP.imgix);
+    });
+
+    hooks.afterEach(function() {
+      config.APP.imgix = this.initialAppConfig;
+    });
+
+    module('debug params', function() {
+      test('it does not render debug params when passing debug false', async function(assert) {
+        config.APP.imgix.debug = false;
+
+        await render(
+          hbs`
+          <div>
+            <style>.imgix-bg { width: 1250px; height: 200px;}</style>
+            {{#imgix-bg
+              path="/users/1.png"
+            }}
+              Content
+            {{/imgix-bg}}
+          </div>
+          `);
+
+        const debugParams = [
+          'txtalign',
+          'txtclr',
+          'txtfit',
+          'txtfont',
+          'txtpad',
+          'txtsize',
+        ];
+
+        expectSrcsTo(this.$, (_, uri) => debugParams.forEach(debugParam => assert.notOk(uri.hasQueryParam(debugParam))));
+      });
+
+      test('it will render debug params when passing debug true', async function(assert) {
+        config.APP.imgix.debug = true;
+
+        await render(
+          hbs`
+          <div>
+            <style>.imgix-bg { width: 1250px; height: 200px;}</style>
+            {{#imgix-bg
+              path="/users/1.png"
+            }}
+              Content
+            {{/imgix-bg}}
+          </div>
+          `);
+
+        const debugParams = [
+          'txtalign',
+          'txtclr',
+          'txtfit',
+          'txtfont',
+          'txtpad',
+          'txtsize',
+        ];
+
+        expectSrcsTo(this.$, (_, uri) => debugParams.forEach(debugParam => assert.ok(uri.hasQueryParam(debugParam))));
+      });
+    });
+
+    module('default css class', function() {
+      test('it allows overriding the default class via config', async function(assert) {
+        assert.expect(1);
+
+        config.APP.imgix.classNames = 'imgix-is-rad';
+
+        await render(
+          hbs`
+          <div>
+            <style>.imgix-bg { width: 1250px; height: 200px;}</style>
+            {{#imgix-bg
+              path="/users/1.png"
+            }}
+              Content
+            {{/imgix-bg}}
+          </div>
+          `);
+
+        assert.ok(this.$('div').hasClass('imgix-is-rad'));
+      });
+
+      test('the default class given to the rendered element is `imgix-bg`', async function(assert) {
+        assert.expect(1);
+
+        await render(
+          hbs`
+          <div>
+            <style>.imgix-bg { width: 1250px; height: 200px;}</style>
+            {{#imgix-bg
+              path="/users/1.png"
+            }}
+              Content
+            {{/imgix-bg}}
+          </div>
+          `);
+
+        assert.ok(this.$('div').hasClass('imgix-bg'));
+      });
+    });
+
+    module('library param', function() {
+      test('it adds the library param to all srcs by default', async function(assert) {
+        await render(
+          hbs`
+          <div>
+            <style>.imgix-bg { width: 1250px; height: 200px;}</style>
+            {{#imgix-bg
+              path="/users/1.png"
+            }}
+              Content
+            {{/imgix-bg}}
+          </div>
+          `);
+
+        expectSrcsTo(this.$, (_, uri) => assert.ok(uri.hasQueryParam('ixlib')));
+      });
+
+      test('it allows disabling the imigx library param via conifg', async function(assert) {
+        config.APP.imgix.disableLibraryParam = true;
+
+        await render(
+          hbs`
+          <div>
+            <style>.imgix-bg { width: 1250px; height: 200px;}</style>
+            {{#imgix-bg
+              path="/users/1.png"
+            }}
+              Content
+            {{/imgix-bg}}
+          </div>
+          `);
+
+        expectSrcsTo(this.$, (_, uri) => assert.notOk(uri.hasQueryParam('ixlib')));
+      });
+    });
+
+    module('default params from app config', function() {
+      test('are respected ', async function(assert) {
+        config.APP.imgix.defaultParams = {
+          'toBoop': 'the-snoot',
+        };
+
+        await render(
+          hbs`
+          <div>
+            <style>.imgix-bg { width: 1250px; height: 200px;}</style>
+            {{#imgix-bg
+              path="/users/1.png"
+            }}
+              Content
+            {{/imgix-bg}}
+          </div>
+          `);
+
+        expectSrcsTo(this.$, (_, uri) => assert.equal(uri.getQueryParamValue('toBoop'), 'the-snoot'));
+      });
+
+      test('`options` attr takes precedence', async function(assert) {
+        config.APP.imgix.defaultParams = {
+          auto: 'faces',
+        };
+
+        await render(
+          hbs`
+          <div>
+            <style>.imgix-bg { width: 1250px; height: 200px;}</style>
+            {{#imgix-bg
+              path="/users/1.png"
+              options=(hash
+                auto='format'
+              )
+            }}
+              Content
+            {{/imgix-bg}}
+          </div>
+          `);
+
+        expectSrcsTo(this.$, (_, uri) => assert.equal(uri.getQueryParamValue('auto'), 'format'));
+      });
+
+      test('params on the path take precedence', async function(assert) {
+        config.APP.imgix.defaultParams = {
+          auto: 'faces',
+        };
+
+        await render(
+          hbs`
+          <div>
+            <style>.imgix-bg { width: 1250px; height: 200px;}</style>
+            {{#imgix-bg
+              path="/users/1.png?auto=format"
+            }}
+              Content
+            {{/imgix-bg}}
+          </div>
+          `);
+
+        expectSrcsTo(this.$, (_, uri) => assert.equal(uri.getQueryParamValue('auto'), 'format'));
+      });
+    });
+  });
 });
+
+// matcher should be in the form (url: string, uri: URI) => boolean
+// Should work for both w-type srcsets and dpr srcsets
+function expectSrcsTo($, matcher) {
+  const div = $('div').find('.imgix-bg').attr('style');
+  const src = div.split("'")[1];
+  matcher(src, new URI(src));
+}
